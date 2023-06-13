3e6cd958d80d1e63f6d1d93e21136802397db9c2
abhishek <purohitabhishek308@gmail.com>
2023-06-13 17:56:16 +0530

    day3

--- src/components/main-imgs/MainImages.jsx
+++ src/components/main-imgs/MainImages.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {heading, subHeading} from './../../../public/data'
+import { AiFillCaretDown} from 'react-icons/Ai'
 import "./mainimages.css"
 
 const MainImages = () => {
@@ -12,6 +13,39 @@ const MainImages = () => {
           <p>{subHeading}</p>
           <button className = " btn-search">start your journey</button>
         </div>
+        <div className="trip-bx">
+          <div className = "search-bx">
+            <div className="card">
+              <h4>Location<i className="drop-down"><AiFillCaretDown/></i></h4>
+              <input type='text' placeholder = "paris"/>
+            </div>
+            <div className="card">
+              <h4>Date <i className="drop-down"><AiFillCaretDown/></i></h4>
+              <input type="date" />
+            </div>
+            <div className="card">
+              <h4>people </h4>
+              <input type = "number" placeholder='how many persons'/>
+            </div>
+            <input type='button' value ="Explore Now"/>
+          </div>
+          <div className='travel-bx'>
+            <h4>countries to travel</h4>
+            <div className='cards'>
+              <div className="card">
+                <h3>INDIA <img src="" alt=""/></h3>
+                <img src="" alt=""/>
+                <div className="btn-explore">
+                  <a href = "">read Now</a>
+                  <h5>Mumbai Central <br/> <span>$00.00</span></h5>
+                </div>
+              </div>
+              <div className="card"></div>
+              <div className="card"></div>
+              <div className="card"></div>
+            </div>
+          </div>
+         </div>
       </div>
     </>
   )
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,6 +7,7 @@ const MainPage = () => {
   return (
     <>
       <MainImages/>
+    
      
     </>
   )
